254c6ac53c1f8bb00aebbc016ff84e524bab0592
AnushkaWijegoonawardana97 <anushkaduwolka123@gmail.com>
2023-08-01 10:25:10 +0530

    setting up for heroku deployment

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (request, response) => response.send("Api is running..."));
+app.get("/admin", (request, response) => response.send("Api is running..."));
+
+// Server Static Asstes In Production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (request, response) => {
+    response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
